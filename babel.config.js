c461c87a5289b578a2b6639978b104b9090715c0
malaygpt21 <malaygpt21@gmail.com>
2025-01-22 22:08:04 +0530

    Updated Babel configuration for ES modules

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     presets: [
         [
             '@babel/preset-env',
@@ -6,7 +6,22 @@ module.exports = {
                 targets: {
                     node: 'current',
                 },
+                modules: false
             },
         ],
     ],
+    env: {
+        test: {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            node: 'current',
+                        },
+                    },
+                ],
+            ],
+        },
+    },
 };
